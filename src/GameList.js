33e5c54256a1ac6bd81677675c94f44ae4090190
r <rc@gmail.com>
2022-08-11 23:51:55 +0100

    get name from the name box element fix refresh issue, user loses name when it is stored in variable

--- src/GameList.js
+++ src/GameList.js
@@ -3,10 +3,11 @@ import React, { useState, useRef, useEffect } from "react";
 
 
 function GameList( props ) {
-  const [ games, setGames ]    = useState(['']);
+  const [ games, setGames ]    = useState([]);
   const [showPlayerId, setShowPlayerId] = useState(false);
   const [playerId, setPlayerId] = useState('');
   const [showGameId, setShowGameId] = useState(false);
+  const nameBox = useRef(null);
 
   //let playerId ='';
   let gameId   ='';
@@ -24,22 +25,26 @@ function GameList( props ) {
   //let games = '';
   let setName = ( e ) => {
     name = e.target.value ;
-    alert( name );
     //console.log( name );
   }
+  let showGamesAvailable =() => {
+    setGames([]);
+    getGameList();
+    //setGames( games );
+  }
+  let getGameList = () => {
+    if ( games.length < 1 ) {
+    // alert( "refresh data name  outside is is " + nam  );
+      fetch('http://127.0.0.1:8080/wp-json/black-jack/v1/get_game_list/').then( response => response.json())
+      .then( data =>{ console.log( data );
+        setGames( data );
+        //alert( "refresh data name is " + nam );
+        //setThName( nam);
+        return data;
+      }
+      );
 
-  let getGameList = ( nam ) => {
-   // alert( "refresh data name  outside is is " + nam  );
-    fetch('http://127.0.0.1:8080/wp-json/black-jack/v1/get_game_list/').then( response => response.json())
-    .then( data =>{ console.log( data );
-      setGames( data );
-      //alert( "refresh data name is " + nam );
-      setThName( nam);
-      return data;
     }
-    );
-
-
   }
 
   const startGame = ( e ) => {
@@ -83,16 +88,19 @@ function GameList( props ) {
 
   //join games
   let JoinGame = ( e ) =>{
-    props.setTheName( name );
-    alert( "JOIN Game the name is : " + props.getTheName());
-    console.log("JOIN GAME " + props.getTheName() );
-    console.log('++++++ : ' + playerId )
+    //props.setTheName( name );
+    let namebx = document.getElementById("name-box").value;
+
+    //alert( "JOIN Game the name is : " + namebx );
+    //console.log( nameBox.current.focus()  );
+    //console.log("JOIN GAME " + props.getTheName() );
+    //console.log('++++++ : ' + playerId )
     let get_player_url ='';
     if( playerId ) {
       get_player_url = 'http://127.0.0.1:8080/wp-json/black-jack/v1/getplayer/?player_id=' + playerId +'&game_id=' + e.target.id ;
     } else {
-      alert('_______ NAME for add player = ' + props.getTheName() );
-      get_player_url = 'http://127.0.0.1:8080/wp-json/black-jack/v1/addplayer/?game_id=' + e.target.id  + '&player_name=' + props.getTheName();
+      //alert('_______ NAME for add player = ' + props.getTheName() );
+      get_player_url = 'http://127.0.0.1:8080/wp-json/black-jack/v1/addplayer/?game_id=' + e.target.id  + '&player_name=' + namebx;
     }
     fetch( get_player_url ).then( response => response.json())
     .then( data =>{
@@ -116,14 +124,14 @@ function GameList( props ) {
   return (
     <div className="bj-game-list">
       <div className="user-input">
-        <p><label>Enter your player name </label><input onChange = { setName } type="text"></input><span className ="info">&#x1F6C8;</span></p>
+        <p><label>Enter your player name </label><input id="name-box" type="text" ref={ nameBox }></input><span className ="info">&#x1F6C8;</span></p>
         <p><input onClick={ chsetShowPlayerId } type="checkbox"></input> Enter a player ID <label>Player ID </label><input onChange = { setGamePlayerId } type="text" style={ { display: ( showPlayerId )? 'inline-block' : 'none' } } value={ playerId }/><span className ="info">&#x1F6C8;</span></p>
         <p><input onClick={ chsetShowGameId } type="checkbox"></input> Enter a game ID <label>Game ID </label><input onChange = { setGameId } type="text" style={ { display: ( showGameId )? 'inline-block' : 'none' } }/><button style={ {display:( showGameId )? 'inline-block' : 'none' } }> Join </button><span className ="info">&#x1F6C8;</span></p>
       </div>
         <div className="list-data">
         { (games != false) && games.map((e , i) => (<p onClick={ JoinGame } style={ { color:'white'}} key={ i } id={ e.game_id } > Game ID: { e.game_id } Game status: { e.status } Number of players: { e.max_players } Current number of players: { e.players_in_game } Players: { e.players } Winners : { e.winners } </p>) )}
        </div>
-      <div> <button onClick = { getGameList( getName() ) }> Find new games !! </button></div>
+      <div> <button onClick = { showGamesAvailable }> Find new games !! </button></div>
     </div>
   );
 }
